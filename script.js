2a71489ef97da47a4749c30024266a9921f47a1f
cesar navarro <cnavarro86@users.noreply.github.com>
2020-04-09 14:23:25 +1000

    added media queries for card image

--- script.js
+++ script.js
@@ -86,7 +86,7 @@ function startWorkout() {
     startTimer();
 
     document.getElementById("exercise").innerHTML = exString;
-    document.getElementById("img").innerHTML = "<img src='./card_images/" + cards[0] + "' height='500' width='350'>";
+    document.getElementById("img").innerHTML = "<img src='./card_images/" + cards[0] + "' class='card-img'>";
     // speak(exString);
 }
 
@@ -102,7 +102,7 @@ function nextImage() {
         exercise = getExercise(suite);
         exString = getReps(reps) + " " + exercise;
         document.getElementById("exercise").innerHTML = exString;
-        document.getElementById("img").innerHTML = "<img src='./card_images/" + cards[currentIndex + 1] + "' height='500' width='350'>";
+        document.getElementById("img").innerHTML = "<img src='./card_images/" + cards[currentIndex + 1] + "' class='card-img'>";
         // speak(exString);
     }
     else {
@@ -187,13 +187,13 @@ function speak(ex) {
 ///////////////////////////////
 var startTimerButton, pauseTimerButton, timerDisplay;
 var domReady = function(callback) {
-  document.readyState === "interactive" || document.readyState === "complete" ? callback() : document.addEventListener("DOMContentLoaded", callback);
+    document.readyState === "interactive" || document.readyState === "complete" ? callback() : document.addEventListener("DOMContentLoaded", callback);
 };
 
 domReady(function() {
-  startTimerButton = document.querySelector('.startTimer');
-  pauseTimerButton = document.querySelector('.pauseTimer');
-  timerDisplay = document.querySelector('.timer');
+    startTimerButton = document.querySelector('.startTimer');
+    pauseTimerButton = document.querySelector('.pauseTimer');
+    timerDisplay = document.querySelector('.timer');
 });
 
 
@@ -205,72 +205,51 @@ var savedTime;
 var paused = 0;
 var running = 0;
 function startTimer(){
-  if(!running){
-    startTime = new Date().getTime();
-    tInterval = setInterval(getShowTime, 1);
-// change 1 to 1000 above to run script every second instead of every millisecond. one other change will be needed in the getShowTime() function below for this to work. see comment there.   
- 
-    paused = 0;
-    running = 1;
-timerDisplay.style.background = "#FF0000";
-    timerDisplay.style.cursor = "auto";
-    timerDisplay.style.color = "yellow";
-    startTimerButton.classList.add('lighter');
-    pauseTimerButton.classList.remove('lighter');
-    startTimerButton.style.cursor = "auto";
-    pauseTimerButton.style.cursor = "pointer";
-  }
+    if(!running){
+        startTime = new Date().getTime();
+        tInterval = setInterval(getShowTime, 1);
+        // change 1 to 1000 above to run script every second instead of every millisecond. one other change will be needed in the getShowTime() function below for this to work. see comment there.   
+        paused = 0;
+        running = 1;
+    }
 }
+
 function pauseTimer(){
-  if (!difference){
-    // if timer never started, don't allow pause button to do anything
-  } else if (!paused) {
-    clearInterval(tInterval);
-    savedTime = difference;
-    paused = 1;
-    running = 0;
-    timerDisplay.style.background = "#A90000";
-    timerDisplay.style.color = "#690000";
-    timerDisplay.style.cursor = "pointer";
-    startTimerButton.classList.remove('lighter');
-    pauseTimerButton.classList.add('lighter');
-    startTimerButton.style.cursor = "pointer";
-    pauseTimerButton.style.cursor = "auto";
-  } else {
-// if the timer was already paused, when they click pause again, start the timer again
-startTimer();
-  }
+    if (!difference){
+        // if timer never started, don't allow pause button to do anything
+    } else if (!paused) {
+        clearInterval(tInterval);
+        savedTime = difference;
+        paused = 1;
+        running = 0;
+    } else {
+        // if the timer was already paused, when they click pause again, start the timer again
+        startTimer();
+    }
 }
+
 function resetTimer(){
-  clearInterval(tInterval);
-  savedTime = 0;
-  difference = 0;
-  paused = 0;
-  running = 0;
-  timerDisplay.innerHTML = 'Start Timer!';
-  timerDisplay.style.background = "#A90000";
-  timerDisplay.style.color = "#fff";
-  timerDisplay.style.cursor = "pointer";
-  startTimerButton.classList.remove('lighter');
-  pauseTimerButton.classList.remove('lighter');
-  startTimerButton.style.cursor = "pointer";
-  pauseTimerButton.style.cursor = "auto";
+    clearInterval(tInterval);
+    savedTime = 0;
+    difference = 0;
+    paused = 0;
+    running = 0;
 }
+
 function getShowTime(){
-  updatedTime = new Date().getTime();
-  if (savedTime){
+    updatedTime = new Date().getTime();
+    if (savedTime){
     difference = (updatedTime - startTime) + savedTime;
-  } else {
+    } else {
     difference =  updatedTime - startTime;
-  }
-  // var days = Math.floor(difference / (1000 * 60 * 60 * 24));
-  var hours = Math.floor((difference % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-  var minutes = Math.floor((difference % (1000 * 60 * 60)) / (1000 * 60));
-  var seconds = Math.floor((difference % (1000 * 60)) / 1000);
-  var milliseconds = Math.floor((difference % (1000 * 60)) / 100);
-hours = (hours < 10) ? "0" + hours : hours;
-  minutes = (minutes < 10) ? "0" + minutes : minutes;
-  seconds = (seconds < 10) ? "0" + seconds : seconds;
-  milliseconds = (milliseconds < 100) ? (milliseconds < 10) ? "00" + milliseconds : "0" + milliseconds : milliseconds;
-  timerDisplay.innerHTML = hours + ':' + minutes + ':' + seconds + ':' + milliseconds;
+    }
+    var hours = Math.floor((difference % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    var minutes = Math.floor((difference % (1000 * 60 * 60)) / (1000 * 60));
+    var seconds = Math.floor((difference % (1000 * 60)) / 1000);
+    var milliseconds = Math.floor((difference % (1000 * 60)) / 100);
+    hours = (hours < 10) ? "0" + hours : hours;
+    minutes = (minutes < 10) ? "0" + minutes : minutes;
+    seconds = (seconds < 10) ? "0" + seconds : seconds;
+    milliseconds = (milliseconds < 100) ? (milliseconds < 10) ? "00" + milliseconds : "0" + milliseconds : milliseconds;
+    timerDisplay.innerHTML = hours + ':' + minutes + ':' + seconds + ':' + milliseconds;
 }
